da3f7d945f1af150ae79e95a6c4096b56128e3a2
ManuYadav <manuyadav989898@gmail.com>
2026-08-08 21:11:16 +0530

    input_validion_check

---
+++ inputval.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const  app = express(); // created the instanece of the express 
+  
+ app.use(express.json()); // using a express.json middleware to  use post
+   app.post("/health-checkup" , function(req,res){
+    //  input - kidney [1,2]
+    const kidney=  req.body.kidney;
+    const kidneylength =kidney.length;
+    res.send("you have " + kidneylength + " kidneys " );   
+
+   });
+   // if user gave any  invald 
+   // global catches  // error based middlewares defined at the end of program  with 2 arguemnts in funtion 
+   app.use(function(err, req , res , next){
+    res.status(500).json({
+      msg: "Invalid Credentials" //  if user doesnt gave kidney as  array we will se this message 
+      // without expsoing the backened  logic in error 
+
+    });
+
+   });
+    app.listen(3000);
